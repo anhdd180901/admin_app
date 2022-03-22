a3c4a68f93ccff3b5537acb5a5f7fed188cc0bc4
Dao Duy Anh <daoduyanh@NALs-Mac-mini.local>
2022-03-22 15:34:08 +0700

    test helper function

--- app/helpers/librarian_helper.rb
+++ app/helpers/librarian_helper.rb
@@ -1,2 +1,9 @@
 module LibrarianHelper
+    def format_name(librarian)
+        if librarian.age >= 18
+            "Mr. #{librarian.name}"
+        else
+            "#{librarian.name}"
+        end
+    end
 end
